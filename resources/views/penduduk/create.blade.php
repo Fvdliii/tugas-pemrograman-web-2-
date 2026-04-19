2d3ebc9440f29ea61b170c2760d335a1c377aa69
Fvdliii <fgunawan528@gmail.com>
2026-04-20 06:17:23 +0800

    penyiapan blade component untuk layout utama

---
+++ resources/views/penduduk/create.blade.php
@@ -0,0 +1,8 @@
+<x-app>
+
+    <x-slot:title>
+        {{ $title }}
+    </x-slot>
+
+    <h1 class="fw-bold">form create Penduduk</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Penduduk', function () {
+    return view('penduduk.index', ['title' => 'penduduk']);
+});
+
+Route::get('/Penduduk/create', function () {
+    return view('penduduk.create', ['title' => 'Create student']);
+});
