1eb3b8f1b349888987bfd07c8a45d3eb38a4238f
Fvdliii <fgunawan528@gmail.com>
2026-04-25 17:45:46 +0800

    implementasi fitur read untuk[Index.blade.php]

--- resources/views/penduduk/index.blade.php
+++ resources/views/penduduk/index.blade.php
@@ -4,12 +4,29 @@
         {{ $title }}
     </x-slot>
 
-    <ul class="list-group">
-        @foreach ($Penduduks as $penduduk)
-            <li class="list-group-item">{{ $loop->iteration }}. {{ $penduduk->nama }} -- {{ $penduduk->email }} --
-                {{ $penduduk->nohp }} --
-                {{ $penduduk->nik }} -- {{ $penduduk->kelamin }}</li>
-        @endforeach
+    <table class="table table-bordered ">
+        <thead class="table-secondary">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>No HP</th>
+                <th>NIK</th>
+                <th>Kelamin</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($Penduduks as $penduduk)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $penduduk->nama }}</td>
+                    <td>{{ $penduduk->email }}</td>
+                    <td>{{ $penduduk->nohp }}</td>
+                    <td>{{ $penduduk->nik }}</td>
+                    <td>{{ $penduduk->kelamin }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-    </ul>
 </x-app>
